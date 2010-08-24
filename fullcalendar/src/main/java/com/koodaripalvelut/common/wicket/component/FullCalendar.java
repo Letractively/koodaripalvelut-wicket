e2db06e26a373d02a4b1f23bc076b00589d6a19d
kryptt <kryptt@2bc94d2d-6a3c-858b-172c-0a070dec9d44>
2010-08-24 00:20:56 +0000

    Starting work on the full calendar.

---
+++ fullcalendar/src/main/java/com/koodaripalvelut/common/wicket/component/FullCalendar.java
@@ -0,0 +1,24 @@
+package com.koodaripalvelut.common.wicket.component;
+
+import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.model.IModel;
+
+/**
+ * @author rhansen@kitsd.com
+ */
+public class FullCalendar extends Panel
+{
+  private static final long serialVersionUID = 1L;
+
+  private FullCalendar(final String id) {
+    super(id);
+    // TODO Auto-generated constructor stub
+  }
+
+  private FullCalendar(final String id, final IModel<?> model)  {
+    super(id, model);
+    // TODO Auto-generated constructor stub
+  }
+
+
+}
