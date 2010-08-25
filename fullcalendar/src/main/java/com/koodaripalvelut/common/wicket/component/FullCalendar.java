e12c84d5e57fffa97ca21b9f00ed9d8e35f96117
kryptt <kryptt@2bc94d2d-6a3c-858b-172c-0a070dec9d44>
2010-08-25 00:49:40 +0000

    STarting the fullcalendar.

--- fullcalendar/src/main/java/com/koodaripalvelut/common/wicket/component/FullCalendar.java
+++ fullcalendar/src/main/java/com/koodaripalvelut/common/wicket/component/FullCalendar.java
@@ -1,6 +1,12 @@
 package com.koodaripalvelut.common.wicket.component;
 
+import org.apache.wicket.ResourceReference;
+import org.apache.wicket.ajax.AbstractDefaultAjaxBehavior;
+import org.apache.wicket.ajax.AjaxRequestTarget;
+import org.apache.wicket.markup.html.IHeaderResponse;
+import org.apache.wicket.markup.html.internal.HtmlHeaderContainer;
 import org.apache.wicket.markup.html.panel.Panel;
+import org.apache.wicket.markup.html.resources.CompressedResourceReference;
 import org.apache.wicket.model.IModel;
 
 /**
@@ -10,15 +16,59 @@ public class FullCalendar extends Panel
 {
   private static final long serialVersionUID = 1L;
 
-  private FullCalendar(final String id) {
-    super(id);
-    // TODO Auto-generated constructor stub
+  private static ResourceReference JS_JQUERY =
+    new CompressedResourceReference(FullCalendar.class, "jquery/jquery.js");
+  private static ResourceReference JS_JQUERY_UI =
+    new CompressedResourceReference(FullCalendar.class, "jquery/jquery-ui-custom.js");
+  private static ResourceReference JS_FULLCAL =
+    new CompressedResourceReference(FullCalendar.class, "fullcalendar.min.js");
+  private static ResourceReference JS_FULLCAL_DBG =
+    new CompressedResourceReference(FullCalendar.class, "fullcalendar.js");
+
+  private class CalendarEvent extends AbstractDefaultAjaxBehavior {
+
+    @Override
+    protected void respond(final AjaxRequestTarget target) {
+      //TODO Working on it.
+    }
+
   }
 
-  private FullCalendar(final String id, final IModel<?> model)  {
+  public FullCalendar(final String id) {
+    this(id, null);
+  }
+
+  public FullCalendar(final String id, final IModel<?> model) {
     super(id, model);
-    // TODO Auto-generated constructor stub
+    init();
   }
 
+  private void init() {
+  }
+
+  @Override
+  public void renderHead(final HtmlHeaderContainer container) {
+    final IHeaderResponse headerResponse = container.getHeaderResponse();
+    if (includeJQuery()) {
+      headerResponse.renderJavascriptReference(JS_JQUERY);
+    }
+    if (includeJQueryUI()) {
+      headerResponse.renderJavascriptReference(JS_JQUERY_UI);
+    }
+    if (getApplication().getDebugSettings().isAjaxDebugModeEnabled()) {
+      headerResponse.renderJavascriptReference(JS_FULLCAL_DBG);
+    } else {
+      headerResponse.renderJavascriptReference(JS_FULLCAL);
+    }
+    super.renderHead(container);
+  }
+
+  protected boolean includeJQuery() {
+    return true;
+  }
+
+  protected boolean includeJQueryUI() {
+    return true;
+  }
 
 }
