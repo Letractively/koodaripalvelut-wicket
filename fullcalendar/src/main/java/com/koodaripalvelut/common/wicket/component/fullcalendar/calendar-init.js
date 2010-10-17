7c607afde2174929e18b0ef691a9b059a5febe76
kryptt <kryptt@gmail.com>
2010-10-17 20:05:15 +0000

    more metadata for the viewDisplay event

--- fullcalendar/src/main/java/com/koodaripalvelut/common/wicket/component/fullcalendar/calendar-init.js
+++ fullcalendar/src/main/java/com/koodaripalvelut/common/wicket/component/fullcalendar/calendar-init.js
@@ -2,10 +2,10 @@ var Feedback = new (function() {
   function setupSend(xhr) {
     xhr.setRequestHeader("Wicket-Ajax", "true");
     if (typeof(Wicket.Focus.lastFocusId) != "undefined" && Wicket.Focus.lastFocusId != "" && Wicket.Focus.lastFocusId != null)
-        xhr.setRequestHeader("Wicket-FocusedElementId", Wicket.Focus.lastFocusId);                
+        xhr.setRequestHeader("Wicket-FocusedElementId", Wicket.Focus.lastFocusId);
     xhr.setRequestHeader("Accept", "text/xml");
   };
-  
+
   function success(data) {
     var xml = data;
     if (typeof xml == "string") {
@@ -15,7 +15,7 @@ var Feedback = new (function() {
     }
     (new Wicket.Ajax.Call("${feedbackURL}")).loadedCallback(xml);
   };
-  
+
   function call(data, revertFunc) {
     $.ajax({
       url : "${feedbackURL}", type: 'POST', contentType: 'application/json;charset=UTF-8'
@@ -23,37 +23,37 @@ var Feedback = new (function() {
      ,beforeSend : setupSend ,success : success, error : revertFunc
     });
   };
-  
+
   this.forSelect = function(startDate, endDate, allDay, jsEvent, view) {
-    call({"feedbackFor" : "select", "startDate" : startDate, "endDate" : endDate, 
+    call({"feedbackFor" : "select", "startDate" : startDate, "endDate" : endDate,
       "allDay" : allDay}, null);
   };
-  
+
   this.forUnselect = function(view, jsEvent) {
     call({"feedbackFor" : "unselect"}, null);
   };
-  
+
   this.forViewDisplay = function(view) {
-    call({"feedbackFor" : "viewDisplay"}, null);
+    call({"feedbackFor" : "viewDisplay", "startDate" : view.start, "endDate" : view.end, "view" : view.name}, null);
   };
-  
+
   this.forDayClick = function(date, allDay, jsEvent, view) {
     call({"feedbackFor" : "dayClick", "date" : date, "allDay" : allDay}, null);
   };
-  
+
   this.forEvent = function (eventName) {
     return function(event, jsEvent, ui, view) {
       call({"feedbackFor" : eventName, "event" : event}, null);
     };
   };
-  
+
   this.forEventAlter = function (eventName) {
     return function(event, dayDelta, minuteDelta, allDay, revertFunc, jsEvent, ui, view) {
       if (arguments.length == 8)
-        call({"feedbackFor" : eventName, "event" : event, 
+        call({"feedbackFor" : eventName, "event" : event,
           "dayDelta" : dayDelta, "minuteDelta" : minuteDelta, "allDay" : allDay}, revertFunc);
       else
-        call({"feedbackFor" : eventName, "event" : event, 
+        call({"feedbackFor" : eventName, "event" : event,
           "dayDelta" : dayDelta, "minuteDelta" : minuteDelta}, allDay);
     };
   };
